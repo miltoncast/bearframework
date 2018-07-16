d204ca63d6c157e892135462280c81ca35634f60
Milton <10410518+miltonph@users.noreply.github.com>
2018-07-16 23:22:10 +0800

    added error message

--- bearframework/controller/Hello.php
+++ bearframework/controller/Hello.php
@@ -1,16 +1,15 @@
 <?php
 
-
 class Hello extends Controller
 {
 	public function index()
 	{
 		$this->view->name = 'Sharlyn';
-		$this->view->render('home.html');
+		$this->view->render('hello.html');
 	}
 
 	public function greet() {
 		$this->view->name = 'Jomil';
-		$this->view->render('home.html');
+		$this->view->render('hello.html');
 	}
 }
--- bearframework/index.php
+++ bearframework/index.php
@@ -1,26 +1,3 @@
 <?php
-//router
-$tokens = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
-
-//dispatcher
-if (isset($tokens[1]) === true) {
-	$controllerName = ucfirst($tokens[1]);
-	$controllerPath = 'controller/' .  $controllerName . '.php';
-} else {
-	$controllerName = 'Home';
-	$controllerPath = 'controller/Home.php';
-}
-
-if (file_exists($controllerPath) === true) {
-	require_once('libs/Controller.php');
-	require_once($controllerPath);
-	$controller = new $controllerName;
-	
-	if (isset($tokens[2]) === true && method_exists($controller, $tokens[2])) {
-		$action = '' . $tokens[2];
-		$controller->$action();
-	} else {
-		$controller->index();
-	}
-
-}
+require_once('libs/Bootstrap.php');
+$application = new Bootstrap();
